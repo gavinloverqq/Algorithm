0816a370985df2ee8bd6fd3fa392915c705b26b0
macbookAir <779483309@qq.com>
2016-09-21 21:38:14 +0800

    第一次提交

--- algorithm/DP/p1156垃圾陷阱(背包)/main.cpp
+++ algorithm/DP/p1156垃圾陷阱(背包)/main.cpp
@@ -1,47 +1,70 @@
 #include <iostream>
-#define INF 1<<30
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
+struct Node{
+    int T,F,H;
+    Node(){}
+    Node(int t,int f,int h):T(t),F(f),H(h){}
+
+    friend bool operator < (Node a,Node b){
+        return a.T < b.T;
+    }
+};
+
+
 int main() {
-    int v,e;
-    int g[100][100];
-    int a,b,c;
-    cin >> v >> e;
-    for (int i = 0; i <= v; ++i) {
-        for (int j = 0; j <= v; ++j) {
-            g[i][j] = INF;
-            g[i][i] = 0;
+    int D,G;//D 井深度,G垃圾数量
+    Node N[110];
+    cin >> D >> G;
+    int T,F,H;
+    for (int i = 1; i <= G; ++i) {
+        cin >> T >> F >> H;
+        N[i] = Node(T,F,H);
+    }
+    sort(N+1,N+G);
+//    for (int k = 1; k <= G; ++k) {
+//        cout << N[k].T <<" "<<N[k].F<<" "<<N[k].H<<endl;
+//    }
+    int num = 1;int life = 10;
+    for (int k = 1; k <= G; ++k) {
+        if(N[k].T <= life){
+            life += N[k].F;
+            num = k;//饿死
+        } else {
+            break;
         }
     }
-    for (int k = 0; k < e; ++k) {
-        cin >> a >> b >> c;
-        g[a][b] = c;
-//        g[b][a] = c;
-    }
-    int dist[100],book[100];
-    for (int m = 1; m <= v; ++m) {
-        book[m] = 0;
-        dist[m] = g[1][m];
-    }
-    book[1] = 1;
-    for (int l = 1; l < v; ++l) {
-        int minD = INF;int currNode;
-        for (int i = 1; i <= v; ++i) {
-            if(book[i] != 1 && minD > dist[i]){
-                minD = dist[i];
-                currNode = i;
+//    cout << num;
+    int dp[110];int dpLife = 10;
+    memset(dp,0, sizeof(dp));
+    int i;
+    dp[0] = 10;
+//    for (int j = 1; j <= num; ++j) {
+//        for (i = dpLife; i >= N[j].T; --i) {
+//            if(dp[i - N[j].T] > dp[i] + N[j].H){
+//                dp[i] = dp[i - N[j].T];
+//                dpLife += N[j].F;
+//            } else {
+//                dp[i] = dp[i] + N[j].H;
+//            }
+//        }
+//        if(dp[i] >= D) {cout << i << endl;
+//            return 0;
+//        }
+//    }
+    for (int j = 1; j <= G; ++j) {
+        for (i = D; i >= 0; --i) {
+            if(dp[i] < N[j].T) continue;
+            if(i + N[j].H >= D){cout << N[j].T << endl;
+                return 0;
             }
-        }
-        book[currNode] = 1;
-        for (int j = 1; j <= v; ++j) {
-            if(book[j] != 1 && dist[j] > dist[currNode] + g[currNode][j]){
-                dist[j] = dist[currNode] + g[currNode][j];
-            }
+            dp[i + N[j].H] = max(dp[i + N[j].H] ,dp[i]);
+            dp[i] += N[j].F;
         }
     }
-    for (int n = 1; n <= v; ++n) {
-        cout << dist[n] <<" ";
-    }
-    cout << endl;
+    cout << life << endl;
     return 0;
 }

d07d7e3beae48a5826bc64578ef8d53a62d0ae66
macbookAir <779483309@qq.com>
2016-09-23 13:46:31 +0800

    有一处小错误需注意

--- algorithm/DP/p1156垃圾陷阱(背包)/main.cpp
+++ algorithm/DP/p1156垃圾陷阱(背包)/main.cpp
@@ -14,7 +14,6 @@ struct Node{
     }
 };
 
-
 int main() {
     int D,G;//D 井深度,G垃圾数量
     Node N[110];
@@ -37,46 +36,91 @@ int main() {
             break;
         }
     }
-//    cout << num;
-    int dp[110];int dpLife = 10;
-//    memset(dp,0, sizeof(dp));
-    int i;
-    dp[0] = 10;
-//    for (int j = 1; j <= num; ++j) {
-//        for (i = dpLife; i >= N[j].T; --i) {
-//            if(dp[i - N[j].T] > dp[i] + N[j].H){
-//                dp[i] = dp[i - N[j].T];
-//                dpLife += N[j].F;
-//            } else {
-//                dp[i] = dp[i] + N[j].H;
-//            }
-//        }
-//        if(dp[i] >= D) {cout << i << endl;
-//            return 0;
-//        }
-//    }
 
-//    for (int j = 1; j <= G; ++j) {
-//        for (i = D; i >= 0; --i) {
-//            if(dp[i] < N[j].T) continue;
-//            if(i + N[j].H >= D){cout << N[j].T << endl;
-//                return 0;
-//            }
-//            dp[i + N[j].H] = max(dp[i + N[j].H] ,dp[i]);
-//            dp[i] += N[j].F;
-//        }
-//    }
+    //思路一:每个状态依赖前一个状态
+    // dp[i][j] : 表示考虑到第k个垃圾时,寿命为i,高度为j的时候,牛的生存状态
+    //dp[i][j + N[k].H] = true 此垃圾能用
+    //dp[i+N[k].F][j] = true 吃垃圾能吃
+/*
+    bool dp[6000][400] = {false};
+    for (int l = 0; l < 6000; ++l) {
+        for (int i = 0; i < 400; ++i) {
+            dp[l][i] = false;
+        }
+    }
+    dp[10][0] = true;
     for (int j = 1; j <= G; ++j) {
-        for (i = D; i >= 0; --i) {
+        for (int i = 6000-10; i >= N[j].T; --i) {
+            for (int k = 2*D; k >= 0; --k) {
+                if(dp[i][k]){
+                    dp[i][k+N[j].H] = true;
+                    dp[i+N[j].F][k] = true;
+                    if(k + N[j].H >= D) {
+                        cout << N[j].T << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+*/
+
+    //思路二:后一个结果依赖前一个
+    //dp[j] : 表示高度为j时的寿命
+    //dp[j+N[i].H] = max(dp[j],dp[j+N[i].H]) 不吃这个垃圾
+    //dp[j] = dp[j] + N[i].F;
+/*    int dp[110];
+    memset(dp,0, sizeof(dp));
+    dp[0] = 10;
+    for (int j = 1; j <= G; ++j) {
+        for (int i = D; i >= 0; --i) {
             if(dp[i] < N[j].T) continue;
             if(i + N[j].H >= D){cout << N[j].T << endl;
                 return 0;
             }
-            dp[i + N[j].H] =max(dp[i],dp[i + N[j].H]);
+            dp[i + N[j].H] = max(dp[i],dp[i + N[j].H]);
 //            dp[i + N[j].H] = dp[i];
             dp[i] += N[j].F;
         }
-    }
+    }*/
+
+    //思路三:未实现,按照背包问题处理
+//    int dp[1000];
+//    memset(dp,0, sizeof(dp));
+//    dp[0] = 10;
+//    for (int j = 1; j <= G; ++j) {
+//        for (int i = 20*D; i >= N[j].H; --i) {
+//            if(dp[i] < N[j].T) continue;
+//            dp[i] = max(dp[i] + N[j].F,dp[i - N[j].H]);
+//            if(dp[i] >= N[j].T && i >= D){cout << N[j].T << endl;
+//                return 0;
+//            }
+//        }
+//    }
+
+//思路三:未实现,按照背包问题处理
+//    for (int l = 0; l < 2500; ++l) {
+//        dp[l] = -1000000000;
+//    }
+//    dp[0] = 10;int tmp;
+//    for (int j = 1; j <= G; ++j) {
+//        for (int i = 2500; i >= 0; --i) {
+////            tmp = dp[i];
+////            if(i >= N[j].H)
+//                if(i >= N[j].H && dp[i - N[j].H] >= 0 && dp[i] >= 0)
+//                    dp[i] = max(dp[i] + N[j].F,dp[i - N[j].H]);
+//                else if(i >= N[j].H && dp[i - N[j].H] >= 0 && dp[i] < 0)
+//                    dp[i] = dp[i - N[j].H];
+//                else if(dp[i] >= 0)
+//                    dp[i] = dp[i] + N[j].F;
+//                else
+//                    dp[i] = -1000000000;
+//            if(dp[i] < N[j].T) {continue;}
+//            if(i >= D){cout << N[j].T << endl;
+//                return 0;
+//            }
+//        }
+//    }
     cout << life << endl;
     return 0;
 }

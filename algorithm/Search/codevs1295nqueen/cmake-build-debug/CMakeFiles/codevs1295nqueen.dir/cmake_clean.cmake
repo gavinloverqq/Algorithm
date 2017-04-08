f598b59ce8e6ed1d0075e91cb1795dfd953be4b3
macbookAir <779483309@qq.com>
2017-04-08 22:58:34 +0800

    commit

---
+++ algorithm/Search/codevs1295nqueen/cmake-build-debug/CMakeFiles/codevs1295nqueen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codevs1295nqueen.dir/main.cpp.o"
+  "codevs1295nqueen.pdb"
+  "codevs1295nqueen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codevs1295nqueen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ algorithm/Search/codevs1295nqueen/cmake-build-debug/CMakeFiles/codevs1295nqueen.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for codevs1295nqueen.
+# This may be replaced when dependencies are built.
